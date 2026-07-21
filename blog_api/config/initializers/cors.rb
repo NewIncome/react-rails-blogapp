e23e3658a62c8df5eaf7b6b81578581642107822
Alfredo C <alfredo_cardenas@live.com>
2026-07-20 21:59:25 -0600

    feat: -react- add the PostList component - fetches the rails api via the URL env variable - saves the data from the fetch to the state - renders the posts data - fix URL in rails API cors.rb

--- blog_api/config/initializers/cors.rb
+++ blog_api/config/initializers/cors.rb
@@ -9,8 +9,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # Where the react URL goes
-    origins 'example.com'
+    # Where the react URL goes, like http://127.0.0.1:5173  for localhost
+    origins 'localhost:5173'
 
     resource '*',
              headers: :any,
